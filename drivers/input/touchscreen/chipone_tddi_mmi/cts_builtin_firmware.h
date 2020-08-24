3d8ce715b81025dbbd19024026fedfb88b6418f9
hujing3 <hujing3@lenovo.com>
2020-08-24 13:52:39 +0800

    Gin: 3rd panel new TP ICNL9911C bringup
    Gin: 3rd tianma panel+chipone ICNL9911C TP
    touch driver bringup for chipone ICNL9911C
    
    Change-Id: I1e79299cbe94723a17a25412c48ff8e56b7fee0f
    Reviewed-on: https://gerrit.mot.com/1732563
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/chipone_tddi_mmi/cts_builtin_firmware.h
+++ drivers/input/touchscreen/chipone_tddi_mmi/cts_builtin_firmware.h
@@ -5629,12 +5629,22 @@ static u8 icnl9911s_driver_builtin_firmware[] = {
   0x2A, 0x01, 0xB4, 0x01, 0x28
 };
 
+static u8 icnl9911c_driver_builtin_firmware[] = {
+};
+
 const static struct cts_firmware cts_driver_builtin_firmwares[] = {
-  {
+   {
    .name = "OEM-Project",	/* MUST set non-NULL */
    .hwid = CTS_DEV_HWID_ICNL9911S,
    .fwid = CTS_DEV_FWID_ICNL9911S,
    .data = icnl9911s_driver_builtin_firmware,
    .size = ARRAY_SIZE (icnl9911s_driver_builtin_firmware),
+   },
+   {
+   .name = "OEM-Project",	/* MUST set non-NULL */
+   .hwid = CTS_DEV_HWID_ICNL9911C,
+   .fwid = CTS_DEV_FWID_ICNL9911C,
+   .data = icnl9911c_driver_builtin_firmware,
+   .size = ARRAY_SIZE(icnl9911c_driver_builtin_firmware),
    },
 };
